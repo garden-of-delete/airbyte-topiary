1632df7f18b98b7b6562f0070d4f3944810b4000
garden-of-delete <tehcrushinator@gmail.com>
2021-08-30 11:29:16 -0700

    boilerplate tests for AirbyteConfigModel

---
+++ tests/test_airbyte_config_model.py
@@ -0,0 +1,37 @@
+import pytest
+from airbyte_config_model import *
+from airbyte_dto_factory import *
+
+
+
+
+def test_airbyte_config_model__write_yaml():
+    assert False
+
+
+def test_apply_to_deployment():
+    assert False
+
+
+def test_wipe_sources():
+    assert False
+
+
+def test_wipe_destinations():
+    assert False
+
+
+def test_validate():
+    assert False
+
+
+def test_validate_sources():
+    assert False
+
+
+def test_validate_destinations():
+    assert False
+
+
+def test_validate_connections():
+    assert False
