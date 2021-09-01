0edb172ca77116044b7510f48d623c2b24014011
garden-of-delete <tehcrushinator@gmail.com>
2021-09-01 08:45:47 -0700

    initial. updated TODO list for the hack today

--- tentacle.py
+++ tentacle.py
@@ -5,7 +5,7 @@ This is a simple, open-source tool designed to help manage Airbyte deployments a
 TODO LIST:
 - (done) Implement "check" routes for source and destination validation
 - (done) Drop description and code supporting the deployment to deployment use case
-- Implement the connection routes, dto class, and all associated functions
+- (in progress) Implement the connection routes, dto class, and all associated functions
 - (done) Restructure main method as a proper controller. All feedback to user should come from controller
 - Finalize the yaml to deployment workflow
     - (done) Add print statements to create_source and create_destination
@@ -13,7 +13,7 @@ TODO LIST:
     - (done) Address modification of existing sources and destinations
     - (done) Clarify all arg processor functions related to this workflow
     - (in progress) implement the --backup option
-    - (stretch): modification of connections
+    - (stretch): modification of existing connections
 - (done) Deployment to yaml workflow
 - (done) Wipe target workflow
 - (done) Validate workflow
@@ -21,9 +21,9 @@ TODO LIST:
 - (done) License
 - Decorators
 - Type hinting
-- Tests!
+- (in progress) Tests!
 - Post 0.1.0
-    - CI
+    - CI (done)
     - Linter
     - Support for multiple workspaces
     - Update deployment workflow
